43486f2d10819d614b7ecc35a498d8135d3cbca6
s <s@160.39.133.161>
2014-10-31 19:43:49 -0400

    added libraries for using browser in windows 8
    started to reorganize html resources

--- src/aves/dpt/impl/viewers/DataViewerImpl.java
+++ src/aves/dpt/impl/viewers/DataViewerImpl.java
@@ -250,7 +250,7 @@ public class DataViewerImpl extends JPanel implements DataViewer, KeyListener {
 //			use classloader instead of File            
 //          bIm = ImageIO.read(getClass().getClassLoader().getResource(uri));
 
-            java.io.File file = new java.io.File("img/"+uri);
+            java.io.File file = new java.io.File("res/"+uri);
             bIm = ImageIO.read(file);
             if (bIm == null) {
                 throw new DataNotFoundException("Image is null");
@@ -282,7 +282,7 @@ public class DataViewerImpl extends JPanel implements DataViewer, KeyListener {
         try {
             s = values.iterator();
 
-            source = "web/" + s.next();
+            source = "res/" + s.next();
 
             if (source == null) {
                 throw new DataNotFoundException("error reading uri");
