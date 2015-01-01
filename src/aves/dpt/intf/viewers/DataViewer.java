f6eca0e6e2d9902a418f8f8d03737995b8f0c145
s <s@192.168.1.128>
2014-12-31 19:04:26 -0500

    cleaned warnings for Javadoc generation

--- src/aves/dpt/intf/viewers/DataViewer.java
+++ src/aves/dpt/intf/viewers/DataViewer.java
@@ -1,3 +1,8 @@
+/**
+ * World Wind is licensed under the NASA Open Source Agreement {@link http://worldwind.arc.nasa.gov/worldwind-nosa-1.3.html}
+ * Aves' extensions to the NASA Worldwind core fall under the GNU GPL V3 {@link http://www.gnu.org/licenses/gpl-3.0.txt}
+ */
+ 
 package aves.dpt.intf.viewers;
 
 import java.util.List;
